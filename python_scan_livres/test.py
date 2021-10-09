d01ab14b63496cb04b6710ed5bd394e502c62f71
crafty <q.archambaultpro@gmail.com>
2021-10-09 19:09:09 +0200

    QA

--- python_scan_livres/test.py
+++ python_scan_livres/test.py
@@ -1,2 +1,13 @@
-with open('python_scan_livres/livres.txt') as f:
-    lines = f.readlines()
+import os
+from os import listdir
+from os.path import isfile, join
+
+# récupérer le chemin du répertoire courant
+chemin_repertoire_git = os.getcwd()
+file = f"{chemin_repertoire_git}\livres.txt"
+print (file)
+
+
+fichiers = [f for f in listdir(chemin_repertoire_git) if isfile(join(chemin_repertoire_git, f))]
+
+print (fichiers)
