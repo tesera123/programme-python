f3cfc290722f4af549ca1fe09dad9f566a2abab4
crafty <q.archambaultpro@gmail.com>
2021-10-28 23:20:40 +0200

    QA

--- python_scan_livres/test.py
+++ python_scan_livres/test.py
@@ -5,7 +5,7 @@ import sqlite3
 from os import listdir
 from bs4 import BeautifulSoup
 from requests.api import request
-
+import numpy as np
 
 # ***************** CHEMIN DE BASE ***************** 
 chemin_repertoire_git = os.getcwd()
@@ -20,9 +20,36 @@ cur = conn.cursor()
 
 #sql = "DELETE FROM MANGA WHERE id = 5"
 #sql = "Select * from MANGA order by livres asc"
-sql = "SELECT prix REPLACE(prix,'€','') FROM MANGA"
+#sql = "SELECT prix REPLACE(prix,'€','') FROM MANGA"
+#cur.execute("ALTER TABLE MANGA DROP COLUMN prix2")
+#cur.execute("ALTER TABLE MANGA ADD COLUMN prix2 interger")
+#cur.execute("ALTER TABLE MANGA RENAME TO TempOldTable;")
+#cur.execute("UPDATE MANGA SET prix = REPLACE(prix, '€', 'a')")
 
-cur.execute("update MANGA set prix=replace(prix,'€','test')")
+cur.execute("SELECT COUNT (*) FROM MANGA")
+rowcount = cur.fetchone()[0]
+print (type(rowcount))
+
+
+i = 0
+price = 0
+temp = 0
+while i < rowcount:
+    cur.execute('SELECT prix FROM MANGA')
+    lecture = cur.fetchall()
+    #print(lecture)
+    modif = list(lecture[i])
+    #modife = modif.replace("€","")
+    modife = [item.replace('€', '') for item in modif]
+    print(modife)
+
+
+    test = 0 + valeur
+
+    #var = np.array(modife,dtype=float)
+    print(test)
+    i = i+1
+
 
 
 #cur.execute(sql)
