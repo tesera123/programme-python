b09b3fe2a64fbd2473afbd10c3179c347e3d9490
crafty <q.archambaultpro@gmail.com>
2022-04-12 23:25:57 +0200

    QA

--- python_bot/python_bot_ben.py
+++ python_bot/python_bot_ben.py
@@ -3,4 +3,8 @@ import discord
 
 client = discord.Client()
 
-client.run("962808574126485554")
+@client.event
+async def on_ready():
+    print("le bot est pret")
+
+client.run("OTYyODA4NTc0MTI2NDg1NTU0.YlM7XA.qeBkH0p-LmXsAz_cQ2UCp9sbFnc")
